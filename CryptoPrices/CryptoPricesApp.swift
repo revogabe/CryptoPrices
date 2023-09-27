2da1642a5eaef13d85b5f920f5d6803ab6cbe9fa
Daniel Gabriel <revogabe.dev@gmail.com>
2023-09-27 00:15:12 -0300

    Initial Commit

---
+++ CryptoPrices/CryptoPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesApp.swift
+//  CryptoPrices
+//
+//  Created by Daniel Gabriel on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
